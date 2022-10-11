a939e3deff4c57799564ac470c11fae54f947805
Harshal141 <harshalmukundapatil@gmail.com>
2022-10-11 23:38:09 +0530

    js

--- public/app.js
+++ public/app.js
@@ -29,3 +29,32 @@ btn.addEventListener('click', () => {
     let varr = document.querySelector('.search').value 
     window.location.href = `./code/${fin_data[varr]}/index.html`;
 })
+
+// -------------------------------------------------------
+// -----------------counter loader------------------------
+// -------------------------------------------------------
+
+// update following values to change counter : issue
+let forks = 12;
+let stars = 12;
+let issues = 12;
+let contributors = 12;
+
+let count1 = document.querySelector('.count1')
+let count2 = document.querySelector('.count2')
+let count3 = document.querySelector('.count3')
+let count4 = document.querySelector('.count4')
+
+// geting counters
+for(let i=0;i<=forks;++i){
+    setTimeout(function(){count1.innerText=i},20+10*i)
+}
+for(let i=0;i<=stars;++i){
+    setTimeout(function(){count2.innerText=i},20+10*i)
+}
+for(let i=0;i<=issues;++i){
+    setTimeout(function(){count3.innerText=i},20+10*i)
+}
+for(let i=0;i<=contributors;++i){
+    setTimeout(function(){count4.innerText=i},20+10*i)
+}
